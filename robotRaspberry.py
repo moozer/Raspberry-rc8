36b21154dff51748c11f87e38d30ece74af995bd
bigmoozer@raspberry <bigmoozer@gmail.com>
2017-09-11 05:47:05 +0000

    refactor and made each motor its own class

--- robotRaspberry.py
+++ robotRaspberry.py
@@ -3,9 +3,35 @@ import RPi.GPIO as GPIO
 import math
 
 class robotRaspberry():
-    _directionMatrix = [[( 0.0, -1.0), (-1.0,  1.0), (0.0, 1.0)],
-        	        [( 1.0,  1.0), ( 0.0,  0.0), (-1.0, -1.0)],
-	                [(-1.0,  0.0), ( 1.0, -1.0), (1.0, 0.0)] ]
+#    _directionMatrix = [[( 0.0, -1.0), (-1.0,  1.0), (0.0, 1.0)],
+#        	        [( 1.0,  1.0), ( 0.0,  0.0), (-1.0, -1.0)],
+#	                [(-1.0,  0.0), ( 1.0, -1.0), (1.0, 0.0)] ]
+
+    class motor():
+        _motorDirection = { 'forward': (0, 1), 'backwards': (1,0) }
+        def __init__( self, pinConfig, pwm_freq ):
+            self._pinConfig = pinConfig
+            self._pwm_freq = pwm_freq
+	    self._motor = self._setupMotorPins()
+	
+
+        def _setupMotorPins( self ):
+            GPIO.setup( self._pinConfig["dir_A"], GPIO.OUT )
+            GPIO.setup( self._pinConfig["dir_B"], GPIO.OUT )
+            GPIO.setup( self._pinConfig["pwm"], GPIO.OUT )
+            return GPIO.PWM( self._pinConfig["pwm"], self._pwm_freq )
+
+        def setMotorSpeed( self, speed ):
+            if speed > 0:
+	        self._setDirectionPins( self._motorDirection['forward'] )
+	        self._motor.start( speed )
+	    else:
+	        self._setDirectionPins( self._motorDirection['backwards'] )
+	        self._motor.start( -1*speed )
+
+	def _setDirectionPins( self, dPins ):
+            GPIO.output( self._pinConfig["dir_A"], dPins[0] )
+            GPIO.output( self._pinConfig["dir_B"], dPins[1] )
 
 
     def __init__( self, pinConfig, motorConfig ):
@@ -14,20 +40,12 @@ class robotRaspberry():
 
         # set pin for output
         self._motorConfig = motorConfig
-        self._pinConfigA = pinConfig["motorA"]
-        self._pinConfigB = pinConfig["motorB"]
 
-    	self._motorA = self._setupMotorPins( self._pinConfigA )
-    	self._motorB = self._setupMotorPins( self._pinConfigB )
+    	self._motorA = self.motor( pinConfig['motorA'], motorConfig['freq'] )
+    	self._motorB = self.motor( pinConfig['motorB'], motorConfig['freq'] )
 
         self._speed = 0
         self._turnSpeed = (0,0)
-
-    def _setupMotorPins( self, config ):
-        GPIO.setup( config["dir_A"], GPIO.OUT )
-        GPIO.setup( config["dir_B"], GPIO.OUT )
-        GPIO.setup( config["pwm"], GPIO.OUT )
-        return GPIO.PWM( config["pwm"], self._motorConfig["freq"] )
 
     def setSpeed( self, speed ):
         ''' "speed" is actually the pwm duty cycle
@@ -40,14 +58,17 @@ class robotRaspberry():
 	print "- motor A: base %0.1f, turn %0.1f"%( self._speed, self._turnSpeed[0] )
 	print "- motor B: base %0.1f, turn %0.1f"%( self._speed, self._turnSpeed[1] )
 
-        self._motorA.start( self._speed + self._turnSpeed[0] )
-        self._motorB.start( self._speed + self._turnSpeed[1] )
-        return
-
+	self._motorA.setMotorSpeed( self._speed + self._turnSpeed[0] )
+	self._motorB.setMotorSpeed( self._speed + self._turnSpeed[1] )
+	return
+	
     def setDirection( self, turnSpeed ):
         self._turnSpeed = (turnSpeed, -1*turnSpeed)
         self._updateSpeed()
 
+
+#    def _setDirectionPins( self, motor, pins ):
+	
 
     # def updateDirection( self, x, y ):
     # 	int_x = int( round( x, 0 ) )
