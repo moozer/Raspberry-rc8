f676f9563d0ecddc7bd3cab31f79176726ec76a9
moz <moz@local>
2017-09-12 11:29:19 +0200

    importing old robot - first commit

---
+++ robotRaspberry.py
@@ -0,0 +1,78 @@
+from rcGamepad import rcGamepad
+import RPi.GPIO as GPIO
+import math
+
+class raspBot():
+    _directionMatrix = [[( 0.0, -1.0), (-1.0,  1.0), (0.0, 1.0)],
+        	        [( 1.0,  1.0), ( 0.0,  0.0), (-1.0, -1.0)],
+	                [(-1.0,  0.0), ( 1.0, -1.0), (1.0, 0.0)] ]
+
+
+    def __init__( self, pinConfig, motorConfig ):
+        # set the pin numbering scheme
+        GPIO.setmode(GPIO.BCM)
+
+        # set pin for output
+        self._motorConfig = motorConfig
+        self._pinConfigA = pinConfig["motorA"]
+        self._pinConfigB = pinConfig["motorB"]
+
+	self._motorA = self._setupMotorPins( self._pinConfigA )
+	self._motorB = self._setupMotorPins( self._pinConfigB )
+
+        self._speed = self._motorConfig["speed"]
+
+    def _setupMotorPins( self, config ):
+        GPIO.setup( config["dir_A"], GPIO.OUT )
+        GPIO.setup( config["dir_B"], GPIO.OUT )
+        GPIO.setup( config["pwm"], GPIO.OUT )
+        return GPIO.PWM( config["pwm"], self._motorConfig["freq"] )
+
+
+    def setSpeed( self, speed ):
+        print "speed", speed
+        return
+
+    def updateDirection( self, x, y ):
+	int_x = int( round( x, 0 ) )
+	int_y = int( round( y, 0 ) )
+	print "input was %f, %f - rounding to %d, %d - (A,B) = %s"%( x, y, int_x, int_y, self._directionMatrix[int_x+1][int_y+1] )
+        self._setMotorA( self._speed, self._directionMatrix[int_x+1][int_y+1][0] )
+        self._setMotorB( self._speed, self._directionMatrix[int_x+1][int_y+1][1] )
+
+    def _setMotorA( self, speed, weight ):
+        if weight == 0:
+            self._motorA.stop()
+        else:
+            GPIO.output( self._pinConfigA["dir_A"], 0 if weight > 0 else 1 )
+            GPIO.output( self._pinConfigA["dir_B"], 1 if weight > 0 else 0 )
+            self._motorA.start( speed*math.fabs( weight ) )
+
+    def _setMotorB( self, speed, weight ):
+        if weight == 0:
+            self._motorB.stop()
+        else:
+            GPIO.output( self._pinConfigB["dir_A"], 0 if weight > 0 else 1 )
+            GPIO.output( self._pinConfigB["dir_B"], 1 if weight > 0 else 0 )
+            self._motorB.start( speed*math.fabs( weight ))
+
+    def cleanup( self ):
+        self._motorA.stop()
+        self._motorB.stop()
+        GPIO.cleanup()
+
+
+
+if __name__ == "__main__":
+    motorConfig = { "freq": 50, "speed": 65}
+    pinConfig =   { "motorA": {"dir_A": 14, "dir_B": 15, "pwm": 18 },
+                    "motorB": {"dir_A": 3,  "dir_B": 2,  "pwm": 4 }}
+
+    print "using configs"
+    print motorConfig
+    print pinConfig
+
+    robot = robotRaspberry( pinConfig, motorConfig )
+    gp = rcGamepad( robot )
+    gp.printJoystickInfo()
+    gp.runLoop()
